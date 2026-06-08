05b54d1a04ae084ed9c8ae033088ac6ea652cb20
Faizan-dev <ahmedfaizan931star@gmail.com>
2026-06-08 07:31:15 +0000

    Clean repository and verify polished UI build

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_synthbyte_scanmate_ScanMateApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.synthbyte.scanmate.ScanMateApplication"
-)
-public final class _com_synthbyte_scanmate_ScanMateApplication {
-}
